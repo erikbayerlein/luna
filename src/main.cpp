8f8becf32f87f0b28fd6e56eda94133204bae36b
Erik Bayerlein <erik.bayer@icloud.com>
2024-09-15 10:34:22 -0300

    adjusting

--- src/main.cpp
+++ src/main.cpp
@@ -47,7 +47,7 @@ int main() {
 
   // glfw window creation
   // --------------------
-GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Luna", NULL, NULL);
+  GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Luna", NULL, NULL);
   if (window == NULL) {
     std::cout << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
